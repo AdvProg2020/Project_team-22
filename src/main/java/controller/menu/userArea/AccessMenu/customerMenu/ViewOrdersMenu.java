22f429cba8b69fc677c9f9264eb520a8fc67d2a1
alireza1000 <62209354+alireza1000@users.noreply.github.com>
2020-05-18 16:31:19 -0700

    PurchaseMenu init

--- src/main/java/controller/menu/userArea/AccessMenu/customerMenu/ViewOrdersMenu.java
+++ src/main/java/controller/menu/userArea/AccessMenu/customerMenu/ViewOrdersMenu.java
@@ -2,8 +2,21 @@ package main.java.controller.menu.userArea.AccessMenu.customerMenu;
 
 import main.java.controller.menu.Menu;
 
+import java.util.HashMap;
+
 public class ViewOrdersMenu extends Menu {
     public ViewOrdersMenu(Menu parentMenu) {
         super("view orders", parentMenu);
+        HashMap<Integer ,  Menu> subMenu = new HashMap<>( ) ;
+        subMenu.put( 1 , getShowOrderMenu()) ;
+        subMenu.put(2 , getRateMenu()) ;
+    }
+
+    private Menu getShowOrderMenu() {
+        return  null ;
+    }
+
+    private Menu getRateMenu() {
+        return null ;
     }
 }
