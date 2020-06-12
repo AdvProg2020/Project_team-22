270230ca35615c85d9764bbd15249b2d22d04bfa
alireza1000 <62209354+alireza1000@users.noreply.github.com>
2020-06-12 14:51:44 -0700

    product package junit coverage: 88 all method cover except : toString addComment getCategory & setCategory

---
+++ test/java/model/off/OffTest.java
@@ -0,0 +1,9 @@
+package model.off;
+
+import model.product.Product;
+
+public class OffTest {
+
+    Product product = new Product("milk", "PAK", "9000",
+            "food", "fresh");
+}
--- test/java/model/product/ProductTest.java
+++ test/java/model/product/ProductTest.java
@@ -1,9 +1,13 @@
 package model.product;
 
-import model.product.Product;
+import model.Category;
+import model.account.Account;
+import model.account.Role;
+import model.comment.Comment;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class ProductTest {
     Product product = new Product("milk", "PAK", "9000",
@@ -13,25 +17,118 @@ public class ProductTest {
     public void getNameTest() {
         Assert.assertEquals ("milk", product.getName());
     }
+    @Test
+    public void setNameTest() {
+        product.setName("milk2");
+        Assert.assertEquals ("milk2", product.getName());
+    }
 
     @Test
     public void getBrandTest() {
         Assert.assertEquals("PAK" , product.getBrand());
+    }
+    @Test
+    public void setBrandTest() {
+        product.setBrand( "PAK2");
+        Assert.assertEquals("PAK2" , product.getBrand());
     }
 
     @Test
     public void getPriceTest(){
         Assert.assertEquals( 9000  , product.getPrice());
     }
+    @Test
+    public void setPriceTest(){
+        product.setPrice(20000);
+        Assert.assertEquals( 20000  , product.getPrice());
+    }
 
     @Test
     public void getCategoryTest(){
         Assert.assertEquals( "food" , product.getCategoryName());
     }
-
+    @Test
+    public void setCategoryTest(){
+        product.setCategoryName("food2");
+        Assert.assertEquals(
+                "food2" , product.getCategoryName());
+    }
     @Test
     public void getDescriptionTest(){
-//        product.setDescription("fresh");
         Assert.assertEquals( "fresh" , product.getDescription());
     }
+    @Test
+    public void setDescriptionTest(){
+        product.setDescription("fresh2");
+        Assert.assertEquals( "fresh2" , product.getDescription());
+    }
+
+    @Test
+    public void productIdTest(){
+        product.setProductId( "12");
+        Assert.assertEquals( "12"  , product.getProductId());
+    }
+
+    @Test
+    public void  salesmanTest(){
+        Account saleMan = null;
+        try {
+            saleMan = new Account("username",  "firstName", "lastName",  "email@gmail.com",
+            "09101111111",  "1234",  Role.SALESMAN);
+            product.setSalesman( saleMan );
+            Assert.assertEquals( saleMan  , product.getSalesman());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void productStatusTest(){
+        product.setProductStatus( ProductStatus.EDITING);
+        Assert.assertEquals( ProductStatus.EDITING , product.getProductStatus());
+    }
+
+    @Test
+    public void stockStatus(){
+        product.setStockStatus(  StockStatus.AVAILABLE);
+        Assert.assertEquals( StockStatus.AVAILABLE , product.getStockStatus());
+    }
+
+
+//    @Test
+//    public void category(){
+//        product.setCategory( new Category());
+//        Assert.assertEquals( StockStatus.AVAILABLE , product.getStockStatus());
+//    }
+
+    @Test
+    public void pointTest(){
+        try {
+            Account account1 = new Account("username",  "firstName", "lastName",  "email@gmail.com",
+                    "09101111111",  "1234",  Role.SALESMAN);
+            Account account2 = new Account("username2",  "firstName2", "lastName",  "email@gmail.com",
+                    "09101111111",  "1234",  Role.SALESMAN);
+            product.addPointForProduct( account1 , 4);
+            product.addPointForProduct( account2 , 5);
+            Assert.assertEquals( 4.5 , product.getAveragePoint() , 2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void setAndGetcomment(){
+        Comment comment = new Comment( "nice" ,"this is very good product") ;
+        ArrayList<Comment> comments = new ArrayList<>() ;
+        comments.add( comment) ;
+        product.setComments( comments);
+        Assert.assertEquals( comments , product.getComments());
+    }
+
+
+
+
 }
