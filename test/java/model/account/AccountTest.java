3bd78e2d8adc33e087a8d61b641166e6384509d1
alireza1000 <62209354+alireza1000@users.noreply.github.com>
2020-06-14 16:07:39 -0700

    account package update junit coverage: 88 all method cover except : exception in a setter method and toString

--- test/java/model/account/AccountTest.java
+++ test/java/model/account/AccountTest.java
@@ -1,123 +1,156 @@
 package model.account;
 
 
+import model.Discount;
+import model.log.Log;
+import model.log.SellLog;
 import model.product.Product;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 
 public class AccountTest {
-
-    @Test
-    public void getUserNameTest() {
-        Account testAccount = null  ;
+    Account testAccount1 = null  ;
+    Account testAccount2 = null  ;
+    {
         try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
+            testAccount1 = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
+            testAccount2 = new Account("Test", "alireza", "alireza", "agh@gmail.com", "09101111111", "mh:23011", Role.SALESMAN , "company");
         } catch (Exception e) {
 
         }
+    }
 
-        Assert.assertEquals(testAccount.getUsername(), "MHHSS");
+    @Test
+    public void getUserNameTest() {
+        Assert.assertEquals(testAccount1.getUsername(), "MHHSS");
     }
 
     @Test
     public void getFirstNameTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
-
-        }
-        Assert.assertEquals(testAccount.getFirstName(), "mh");
+        Assert.assertEquals(testAccount1.getFirstName(), "mh");
     }
 
     @Test
     public void getLastNameTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
-
-        }
-        ;
-        Assert.assertEquals(testAccount.getLastName(), "soleymani");
+        Assert.assertEquals(testAccount1.getLastName(), "soleymani");
     }
 
     @Test
     public void getEmailTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
 
-        }
-        ;
-        Assert.assertEquals(testAccount.getEmail(), "hssoleymanmh@gmail.com");
+        Assert.assertEquals(testAccount1.getEmail(), "hssoleymanmh@gmail.com");
     }
 
     @Test
     public void getPhoneTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
-
-        }
-        ;
-        Assert.assertEquals(testAccount.getPhone(), "09301395175");
+        Assert.assertEquals(testAccount1.getPhone(), "09301395175");
     }
 
     @Test
     public void getPasswordTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
 
-        }
-        ;
-        Assert.assertEquals(testAccount.getPassword(), "mh:2301");
+        Assert.assertEquals(testAccount1.getPassword(), "mh:2301");
     }
 
     @Test
     public void getRoleTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
-
-        }
-        ;
-        Assert.assertEquals(testAccount.getRole(), Role.CUSTOMER);
+        Assert.assertEquals(testAccount1.getRole(), Role.CUSTOMER);
     }
 
     @Test
     public void getCreditTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
+        testAccount1.setCredit(1000000);
+        Assert.assertEquals(1000000 ,  testAccount1.getCredit() );
+    }
 
-        }
-        ;
-        testAccount.setCredit(1000000);
-        Assert.assertEquals(testAccount.getCredit(), 1000000);
+    @Test
+    public void getComponyName(){
+        Assert.assertEquals( "company" , testAccount2.getCompanyName());
     }
 
     @Test
     public void hasBoughtTheProductTest() {
-        Account testAccount = null;
-        try {
-            testAccount = new Account("MHHSS", "mh", "soleymani", "hssoleymanmh@gmail.com", "09301395175", "mh:2301", Role.CUSTOMER);
-        } catch (Exception e) {
 
-        }
-        ;
-        Assert.assertEquals(testAccount.hasBoughtTheProduct(new Product("milk", "pak", "10000", "food", "fresh")), false);
+        Log log = new Log() ;
+        ArrayList<Log> logs = new ArrayList<>() ;
+        logs.add( log) ;
+        Product product = new Product("milk", "pak", "10000", "food", "fresh") ;
+        ArrayList<Product> products = new ArrayList<>() ;
+        products.add( product) ;
+        log.setProductsList( products);
+        testAccount1.setLogsList( logs);
+        Assert.assertTrue(testAccount1.hasBoughtTheProduct( product));
+        Product product2 = new Product("milk2", "pak", "10000", "food", "fresh") ;
+        Assert.assertFalse(testAccount1.hasBoughtTheProduct( product2));
     }
 
     @Test
-    public void setUsernameTestWithError(){
+    public void discountCode(){
+        String code = "code" ;
+        testAccount1.addDiscountCode(code);
+        Assert.assertEquals( code , testAccount1.getDiscountsCodes().get(0));
+    }
+
+    @Test
+    public void discount(){
+        try {
+            Discount testDiscount = new Discount("2301", LocalDate.of(2020, 12, 1),
+                    LocalDate.of(2022, 1, 1), 20, 100000, 100);
+            Discount testDiscount2 = new Discount("23011", LocalDate.of(2020, 12, 1),
+                    LocalDate.of(2022, 1, 1), 20, 100000, 100);
+
+            testAccount1.addDiscount( testDiscount);
+            testAccount1.addDiscount( testDiscount2);
+            Assert.assertEquals( testDiscount , testAccount1.getDiscountsList().get(0));
+            testAccount1.removeDiscount(testDiscount);
+            Assert.assertEquals( testDiscount2 , testAccount1.getDiscountsList().get(0));
+
+            ArrayList<Discount> discounts = new ArrayList<>() ;
+            discounts.add( testDiscount) ;
+            discounts.add( testDiscount2) ;
+            testAccount1.setDiscountsList( discounts);
+            Assert.assertEquals(  discounts , testAccount1.getDiscountsList());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+
+    public void shopBasket(){
+
+        Product product = new Product("milk", "PAK", "9000",
+                "food", "fresh");
+        Product product2 = new Product("milk2", "PAK", "9000",
+                "food", "fresh");
+        testAccount1.addProductToShopBasket(product);
+        testAccount1.addProductToShopBasket(product);
+        testAccount1.addProductToShopBasket(product2);
+        Assert.assertEquals( 2 , testAccount1.getNumberOfProductInCart(product));
+        Assert.assertTrue( testAccount1.isTheProductInCart( product2));
+        testAccount1.deleteProductFromCart( product2);
+        Assert.assertEquals( 2 , testAccount1.getShopBasket().size());
+
+        ArrayList<Product> shopbasket = new ArrayList<>() ;
+        testAccount1.setShopBasket( shopbasket);
+        Assert.assertEquals( 0 , testAccount1.getShopBasket().size());
+
+    }
+
+    @Test
+    public void log(){
+        Log log = new Log() ;
+        Log sellLog = new SellLog() ;
+        ArrayList<Log> logs = new ArrayList<>() ;
+        logs.add( log) ;
+        logs.add( sellLog) ;
+        testAccount1.setLogsList( logs);
+        Assert.assertEquals( log , testAccount1.getLogsList().get(0));
 
     }
 }
