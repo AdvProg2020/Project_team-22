9625ab79febfaf87901a16f3f8c68ca68e75bf51
alireza1000 <62209354+alireza1000@users.noreply.github.com>
2020-06-26 00:10:03 -0700

    controller for one product in shop basket

---
+++ src/main/java/controller/account/shopBasket/ShopBasketController.java
@@ -0,0 +1,43 @@
+package main.java.controller.account.shopBasket;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import main.java.controller.product.ProductsTileController;
+import main.java.model.databaseUtil.Database;
+import main.java.model.product.Product;
+
+import java.io.IOException;
+
+public class ShopBasketController {
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    public void initialize(){
+        addProduct() ;
+    }
+
+    private void addProduct() {
+
+        for(Product product : Database.getAllProducts()){
+            intiProductTile( product);
+        }
+    }
+
+    public void intiProductTile( Product product){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource( "/main/java/view/account/shopbasket/ShopProduct.fxml"));
+            fxmlLoader.setController(new ProductsTileController( product));
+            Parent root = fxmlLoader.load();
+            vbox.getChildren().add(root) ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/main/java/controller/account/shopBasket/ShopBasketTileController.java
@@ -0,0 +1,72 @@
+package main.java.controller.account.shopBasket;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import main.java.controller.Core;
+import main.java.model.product.Product;
+
+public class ShopBasketTileController {
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private GridPane gride;
+    @FXML
+    private Button increasor;
+    @FXML
+    private Button countLabel;
+    @FXML
+    private Button decreasor;
+    @FXML
+    private Label priceLabel;
+    Product product ;
+    Core core ;
+    VBox parentVbox ;
+
+    @FXML
+    private void initialize(){
+        intiProductTile() ;
+        initDecreasor() ;
+        initIncreasor() ;
+    }
+
+
+
+    public ShopBasketTileController(Product product , VBox parentVbox) {
+        this.product = product ;
+        this.core = new Core() ;
+        this.parentVbox = parentVbox ;
+    }
+
+    public void intiProductTile(){
+        priceLabel.setText( String.valueOf(product.getPrice()));
+        countLabel.setText("1");
+        nameLabel.setText( product.getName());
+
+    }
+    private void initDecreasor() {
+        decreasor.setOnMousePressed( event -> {
+            int num =  Integer.valueOf(countLabel.getText()) ;
+            if(  num == 1 ){
+                parentVbox.getChildren().remove( gride);
+            } else{
+                num-- ;
+                countLabel.setText( String.valueOf( num));
+            }
+        });
+    }
+    private void initIncreasor() {
+        increasor.setOnMousePressed( event -> {
+            int num =  Integer.valueOf(countLabel.getText()) ;
+            if(  num < product.getNumberOfProduct()){
+                num++ ;
+                countLabel.setText( String.valueOf( num));
+            }
+        });
+    }
+
+
+
+}
